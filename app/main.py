670636b8565ef92e238eab2484409ec10b867a9b
RLVizgarra <rodrigolionvizgarra@gmail.com>
2025-10-01 21:37:31 -0300

    added whatsapp webhook api

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
+import os
 from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import PlainTextResponse
+import uvicorn
 
 import graph
 import reformat
@@ -7,10 +11,30 @@ import whatsapp
 
 
 load_dotenv()
+app = FastAPI()
 
+@app.get("/ping", response_class=PlainTextResponse)
+def ping():
+    return "pong"
 
-if __name__ == "__main__":
-    # TODO: Change location to be user-inputted
+@app.get("/webhook/whatsapp", response_class=PlainTextResponse)
+def verify_webhook_whatsapp(req: Request):
+    params = dict(req.query_params)
+
+    if params.get("hub.mode") == "subscribe" and params.get("hub.verify_token") == os.getenv("WHATSAPP_WEBHOOK_TOKEN") and params.get("hub.challenge"):
+        return params.get("hub.challenge")
+    
+    raise HTTPException(status_code=404, detail="Not Found")
+
+@app.post("/webhook/whatsapp")
+async def handle_whatsapp_webhook(req: Request):
+    json: dict = await req.json()
+    notification = json["entry"][0]["changes"][0]["value"]
+    if "messages" not in notification:
+        return
+    notification = notification["messages"][0]
+    sender = notification["from"]
+    #text = notification["text"]["body"] # TODO: use this to get user input for location
     location = "Mariano Acosta"
 
     latitude, longitude = weather.fetch_coordinates(location)
@@ -18,4 +42,11 @@ if __name__ == "__main__":
     weather_data = reformat.convert_weather_data(weather_raw_data)
     message = reformat.format_weather_message(weather_data, location)
     weather_graph = graph.generate_weather_graph(weather_data, location)
-    whatsapp.send_whatsapp_message(message, weather_graph)
+    whatsapp.send_whatsapp_message(sender, message, weather_graph)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=5000
+    )
