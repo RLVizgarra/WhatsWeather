569d4f6630576f91a542259f239b7a9a71c5864b
RLVizgarra <rodrigolionvizgarra@gmail.com>
2025-10-01 21:21:13 -0300

    2 dumb fixes, by me and by facebook

--- app/whatsapp.py
+++ app/whatsapp.py
@@ -6,7 +6,7 @@ import requests
 def upload_media_to_whatsapp(file_path: str) -> str:
     print("Uploading media to WhatsApp...")
 
-    url = f"https://graph.facebook.com/v22.0/{os.getenv("WHATSAPP_PHONE_NUMER_ID")}/media"
+    url = f"https://graph.facebook.com/v22.0/{os.getenv('WHATSAPP_PHONE_NUMBER_ID')}/media"
     headers = {
         "Authorization": f"Bearer {os.getenv('WHATSAPP_ACCESS_TOKEN')}",
     }
@@ -25,16 +25,19 @@ def upload_media_to_whatsapp(file_path: str) -> str:
     return response.json().get("id")
 
 # Whatsapp API call to send a message
-def send_whatsapp_message(message: str, image_path: str) -> None:
+def send_whatsapp_message(to: str, message: str, image_path: str) -> None:
     print("Sending WhatsApp message...")
 
-    url = f"https://graph.facebook.com/v22.0/{os.getenv("WHATSAPP_PHONE_NUMER_ID")}/messages"
+    if to.startswith("54911"):
+        to = "5411" + to[5:]
+
+    url = f"https://graph.facebook.com/v22.0/{os.getenv('WHATSAPP_PHONE_NUMBER_ID')}/messages"
     headers = {
         "Authorization": f"Bearer {os.getenv('WHATSAPP_ACCESS_TOKEN')}"
     }
     payload = {
         "messaging_product": "whatsapp",
-        "to": os.getenv('WHATSAPP_TO_PHONE_NUMBER'),
+        "to": to,
         "type": "image",
         "image": {
             "id": upload_media_to_whatsapp(image_path),
